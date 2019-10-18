347fde9eb9dec06b88315fdccdd651e7b27b3bc2
Jacob Grant <jacobrgrant@gmail.com>
2019-10-18 14:35:52 -0400

    adding 268 model

---
+++ 228/storage_mod_value.m
@@ -0,0 +1,6 @@
+function A = storage_mod_value(B)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+A = 2.245E-07*B^4 - 0.000238719*B^3 + 0.0897835866*B^2 - 13.9900960980*B + 858.8133339992;
+end
+
